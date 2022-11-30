5a6515791349b415dcf6a64a3b9deff45e773615
xuejian113517 <119289973+xuejian113517@users.noreply.github.com>
2022-11-30 16:06:18 +0800

    第一次提交

---
+++ rxandroidble/src/main/java/com/polidea/rxandroidble2/internal/connection/DisconnectionRouter.java
@@ -0,0 +1,172 @@
+package com.polidea.rxandroidble2.internal.connection;
+
+
+import com.polidea.rxandroidble2.RxBleAdapterStateObservable;
+import com.polidea.rxandroidble2.exceptions.BleDisconnectedException;
+import com.polidea.rxandroidble2.exceptions.BleException;
+import com.polidea.rxandroidble2.exceptions.BleGattException;
+import com.polidea.rxandroidble2.internal.DeviceModule;
+import com.polidea.rxandroidble2.internal.RxBleLog;
+import com.polidea.rxandroidble2.internal.util.RxBleAdapterWrapper;
+
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.Queue;
+
+import bleshadow.javax.inject.Inject;
+import bleshadow.javax.inject.Named;
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Cancellable;
+import io.reactivex.functions.Consumer;
+import io.reactivex.functions.Function;
+import io.reactivex.functions.Predicate;
+
+/**
+ * A class that is responsible for routing all potential sources of disconnection to an Observable that emits only errors.
+ */
+@ConnectionScope
+class DisconnectionRouter implements DisconnectionRouterInput, DisconnectionRouterOutput {
+
+    private static final String TAG = "DisconnectionRouter";
+    private final Queue<ObservableEmitter<BleException>> exceptionEmitters = new ConcurrentLinkedQueue<>();
+    private BleException exceptionOccurred;
+    private Disposable adapterMonitoringDisposable;
+
+    @Inject
+    DisconnectionRouter(
+            @Named(DeviceModule.MAC_ADDRESS) final String macAddress,
+            final RxBleAdapterWrapper adapterWrapper,
+            final Observable<RxBleAdapterStateObservable.BleAdapterState> adapterStateObservable
+    ) {
+        /*
+         The below .subscribe() is only to make the above .cache() to start working as soon as possible.
+         We are not tracking the resulting `Subscription`. This is because of the contract of this class which is supposed to be called
+         when a disconnection happens from one of three places:
+            1. adapterStateObservable: the adapter turning into state other than STATE_ON
+            2. onDisconnectedException
+            3. onGattConnectionStateException
+         One of those events must happen eventually. Then the adapterStateObservable (which uses BroadcastReceiver on a Context) will
+         get unsubscribed. The rest of this chain lives only in the @ConnectionScope context and will get Garbage Collected eventually.
+         */
+        adapterMonitoringDisposable = awaitAdapterNotUsable(adapterWrapper, adapterStateObservable)
+                .map(new Function<Boolean, BleException>() {
+                    @Override
+                    public BleException apply(Boolean isAdapterUsable) {
+                        return BleDisconnectedException.adapterDisabled(macAddress);
+                    }
+                })
+                .firstElement()
+                .subscribe(new Consumer<BleException>() {
+                    @Override
+                    public void accept(BleException exception) {
+                        RxBleLog.d(TAG, "An exception received, indicating that the adapter has became unusable.");
+                        exceptionOccurred = exception;
+                        notifySubscribersAboutException();
+                    }
+                }, new Consumer<Throwable>() {
+                    @Override
+                    public void accept(Throwable throwable) {
+                        RxBleLog.w(TAG, "Failed to monitor adapter state.", throwable);
+                    }
+                });
+    }
+
+    private static Observable<Boolean> awaitAdapterNotUsable(RxBleAdapterWrapper adapterWrapper,
+                                                             Observable<RxBleAdapterStateObservable.BleAdapterState> stateChanges) {
+        return stateChanges
+                .map(new Function<RxBleAdapterStateObservable.BleAdapterState, Boolean>() {
+                    @Override
+                    public Boolean apply(RxBleAdapterStateObservable.BleAdapterState bleAdapterState) {
+                        return bleAdapterState.isUsable();
+                    }
+                })
+                .startWith(adapterWrapper.isBluetoothEnabled())
+                .filter(new Predicate<Boolean>() {
+                    @Override
+                    public boolean test(Boolean isAdapterUsable) {
+                        return !isAdapterUsable;
+                    }
+                });
+    }
+
+    /**
+     * @inheritDoc
+     */
+    @Override
+    public void onDisconnectedException(BleDisconnectedException disconnectedException) {
+        onExceptionOccurred(disconnectedException);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    @Override
+    public void onGattConnectionStateException(BleGattException disconnectedGattException) {
+        onExceptionOccurred(disconnectedGattException);
+    }
+
+    private synchronized void onExceptionOccurred(BleException exception) {
+        if (exceptionOccurred == null) {
+            exceptionOccurred = exception;
+            notifySubscribersAboutException();
+        }
+    }
+
+    private void notifySubscribersAboutException() {
+        if (adapterMonitoringDisposable != null) {
+            adapterMonitoringDisposable.dispose();
+        }
+
+        while (!exceptionEmitters.isEmpty()) {
+            final ObservableEmitter<BleException> exceptionEmitter = exceptionEmitters.poll();
+            exceptionEmitter.onNext(exceptionOccurred);
+            exceptionEmitter.onComplete();
+        }
+    }
+
+    /**
+     * @inheritDoc
+     */
+    @Override
+    public Observable<BleException> asValueOnlyObservable() {
+        return Observable.create(new ObservableOnSubscribe<BleException>() {
+            @Override
+            public void subscribe(final ObservableEmitter<BleException> emitter) {
+                synchronized (DisconnectionRouter.this) {
+                    if (exceptionOccurred != null) {
+                        emitter.onNext(exceptionOccurred);
+                        emitter.onComplete();
+                    } else {
+                        storeEmitterToBeNotifiedInTheFuture(emitter);
+                    }
+                }
+            }
+        });
+    }
+
+    private void storeEmitterToBeNotifiedInTheFuture(final ObservableEmitter<BleException> emitter) {
+        exceptionEmitters.add(emitter);
+        emitter.setCancellable(new Cancellable() {
+            @Override
+            public void cancel() {
+                exceptionEmitters.remove(emitter);
+            }
+        });
+    }
+
+    /**
+     * @inheritDoc
+     */
+    @Override
+    public <T> Observable<T> asErrorOnlyObservable() {
+        return asValueOnlyObservable()
+                .flatMap(new Function<BleException, Observable<T>>() {
+                    @Override
+                    public Observable<T> apply(BleException e) {
+                        return Observable.error(e);
+                    }
+                });
+    }
+}
---
+++ sample/src/main/java/com/polidea/rxandroidble2/sample/example1a_background_scanning/BackgroundScanActivity.java
@@ -0,0 +1,117 @@
+package com.polidea.rxandroidble2.sample.example1a_background_scanning;
+
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.polidea.rxandroidble2.RxBleClient;
+import com.polidea.rxandroidble2.exceptions.BleScanException;
+import com.polidea.rxandroidble2.sample.R;
+import com.polidea.rxandroidble2.sample.SampleApplication;
+import com.polidea.rxandroidble2.scan.ScanFilter;
+import com.polidea.rxandroidble2.scan.ScanSettings;
+
+import java.util.Date;
+import java.util.Locale;
+import java.util.concurrent.TimeUnit;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class BackgroundScanActivity extends AppCompatActivity {
+
+    private static final int SCAN_REQUEST_CODE = 42;
+    private RxBleClient rxBleClient;
+    private PendingIntent callbackIntent;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_example1a);
+        ButterKnife.bind(this);
+        rxBleClient = SampleApplication.getRxBleClient(this);
+        callbackIntent = PendingIntent.getBroadcast(this, SCAN_REQUEST_CODE,
+                new Intent(this, ScanReceiver.class), 0);
+    }
+
+    @OnClick(R.id.scan_start_btn)
+    public void onScanStartClick() {
+
+        try {
+            rxBleClient.getBackgroundScanner().scanBleDeviceInBackground(
+                    callbackIntent,
+                    new ScanSettings.Builder()
+                            .setScanMode(ScanSettings.SCAN_MODE_LOW_LATENCY)
+                            .setCallbackType(ScanSettings.CALLBACK_TYPE_ALL_MATCHES)
+                            .build(),
+                    new ScanFilter.Builder()
+                            .setDeviceAddress("5C:31:3E:BF:F7:34")
+                            // add custom filters if needed
+                            .build()
+            );
+        } catch (BleScanException scanException) {
+            Log.w("BackgroundScanActivity", "Failed to start background scan", scanException);
+            handleBleScanException(scanException);
+        }
+    }
+
+    @OnClick(R.id.scan_stop_btn)
+    public void onScanStopClick() {
+        rxBleClient.getBackgroundScanner().stopBackgroundBleScan(callbackIntent);
+    }
+
+    private void handleBleScanException(BleScanException bleScanException) {
+        final String text;
+
+        switch (bleScanException.getReason()) {
+            case BleScanException.BLUETOOTH_NOT_AVAILABLE:
+                text = "Bluetooth is not available";
+                break;
+            case BleScanException.BLUETOOTH_DISABLED:
+                text = "Enable bluetooth and try again";
+                break;
+            case BleScanException.LOCATION_PERMISSION_MISSING:
+                text = "On Android 6.0 location permission is required. Implement Runtime Permissions";
+                break;
+            case BleScanException.LOCATION_SERVICES_DISABLED:
+                text = "Location services needs to be enabled on Android 6.0";
+                break;
+            case BleScanException.SCAN_FAILED_ALREADY_STARTED:
+                text = "Scan with the same filters is already started";
+                break;
+            case BleScanException.SCAN_FAILED_APPLICATION_REGISTRATION_FAILED:
+                text = "Failed to register application for bluetooth scan";
+                break;
+            case BleScanException.SCAN_FAILED_FEATURE_UNSUPPORTED:
+                text = "Scan with specified parameters is not supported";
+                break;
+            case BleScanException.SCAN_FAILED_INTERNAL_ERROR:
+                text = "Scan failed due to internal error";
+                break;
+            case BleScanException.SCAN_FAILED_OUT_OF_HARDWARE_RESOURCES:
+                text = "Scan cannot start due to limited hardware resources";
+                break;
+            case BleScanException.UNDOCUMENTED_SCAN_THROTTLE:
+                text = String.format(
+                        Locale.getDefault(),
+                        "Android 7+ does not allow more scans. Try in %d seconds",
+                        secondsTill(bleScanException.getRetryDateSuggestion())
+                );
+                break;
+            case BleScanException.UNKNOWN_ERROR_CODE:
+            case BleScanException.BLUETOOTH_CANNOT_START:
+            default:
+                text = "Unable to start scanning";
+                break;
+        }
+        Log.w("EXCEPTION", text, bleScanException);
+        Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
+    }
+
+    private long secondsTill(Date retryDateSuggestion) {
+        return TimeUnit.MILLISECONDS.toSeconds(retryDateSuggestion.getTime() - System.currentTimeMillis());
+    }
+}
